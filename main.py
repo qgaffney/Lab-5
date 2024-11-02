9293e32399e840da13b2e0e3411c2f68ad92ae1b
Quateem Gaffney-Smith <90237467+qgaffney@users.noreply.github.com>
2024-11-02 19:04:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 #Create Class book
 class Book:
   #Provide attributes
-  def __init__(self, title, author, pages, read = False)
+  def __init__(self, title, author, pages, read):
   #Constructor to initializse
-  self.title = title
-  self.author = author
-  self.pages = pages
-  self.read = read
+    self.title = title
+    self.author = author
+    self.pages = pages
+    self.read = False
 
   #Define description() method
   def description(self):
     #Return string
-    return f"Title: {self.title}, Author: {self.author}, Pages {self.pages}"
+    return f"Title: {self.title}, Author: {self.author}, Pages: {self.pages}"
     
   #Define marked_as_read() method
   def marked_as_read(self):
     #Set read to True
     self.read = True
     #Print message
-    print(f"{self.title} has been read.")
+    print(f"{self.title} by {self.author} has been read.")
 
 #Create book object 1
 book1 = Book("Becoming Supernatural: How Common People Are Doing the Uncommon", "Dr. Joe Dispenza", 384)
-#Print descriptions
+#Print description
 print(book1.description())
 #Mark as read
-book1.mark_as_read()
+book1.marked_as_read()
 
 #Create book object 2
 book2 = Book("The Seven Spiritual Laws of Success: A Practical Guide to the Fulfillment of Your Dreams", "Deepak Chopra", 118)
-#Print descriptions
+#Print descriptions=
 print(book2.description())
